acdf79ad34df6e84b45b2067c5894ec6676f02eb
rafaelMachado1891 <rafadmn1@gmail.com>
2025-10-27 15:27:06 -0300

    update

---
+++ bootcamp_python/aula_01/desafio-01.py
@@ -0,0 +1,7 @@
+# crie um programa que o usuário digite o seu nome e retorne o número de caracteres! 
+
+nome = input('Digite o seu nome: ')
+
+resultado = len(nome)
+
+print(f'O nome {nome} possui um total de {resultado} caracteres!')
