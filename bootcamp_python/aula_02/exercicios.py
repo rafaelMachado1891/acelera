dfd590ab6ff9314f45e08c744d3e875f869625c0
rafaelMachado1891 <rafadmn1@gmail.com>
2025-11-02 20:59:01 -0300

    resolucao exercicios 02 e 03

--- bootcamp_python/aula_02/exercicios.py
+++ bootcamp_python/aula_02/exercicios.py
@@ -1,15 +1,29 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-valor_1: int = int(input('Digite o primeiro valor: '))
+# valor_1: int = int(input('Digite o primeiro valor: '))
 
-valor_2: int = int(input('Digite o segundo valor: '))
+# valor_2: int = int(input('Digite o segundo valor: '))
 
-total: int = valor_1 + valor_2
+# total: int = valor_1 + valor_2
 
-print(f'A soma dos valores é {total}')
+# print(f'A soma dos valores é {total}')
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# valor: int = int(input('Digite um valor: '))
+
+# resultado = valor % 5 
+
+# print(f'O resto da divisao de {valor} é {resultado}')
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+valor_01: int = int(input('Digite o primeiro valor: '))
+
+valor_02: int = int(input('Digite o segundo valor: '))
+
+multiplicacao: int = valor_01 * valor_02
+
+print(f'O Resultado da multiplicação de {valor_01} e {valor_02} é {multiplicacao}')
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
